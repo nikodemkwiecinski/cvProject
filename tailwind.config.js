49cc2518c3647940db9a6714a264e07ab9cb2dc4
Nikodem Kwiecinski <nikokwiatek@gmail.com>
2021-07-09 16:54:28 +0200

    nav + footer creation and begining of main section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      lineHeight:{
+        '11': '4rem'
+      },
+      height:{
+        'halfScreen': '50vh'
+      },
+      backgroundColor: theme => ({
+        'myOrange': '#FF9F10',
+        'mainBgc': '#FFF8F8'
+      }),
+      fontFamily:{
+        'body': ['Roboto', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
