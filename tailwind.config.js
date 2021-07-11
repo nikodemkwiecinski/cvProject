becf743d546a526f89c3f178e28294119f47ddc3
Nikodem Kwiecinski <nikokwiatek@gmail.com>
2021-07-11 20:17:44 +0200

    finishing main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,18 @@ module.exports = {
       lineHeight:{
         '11': '4rem'
       },
+      textColor:{
+        'myGray': '#C7BDBD'
+      },
       height:{
-        'halfScreen': '50vh'
+        'halfScreen': '75vh'
       },
       backgroundColor: theme => ({
         'myOrange': '#FF9F10',
         'mainBgc': '#FFF8F8'
+      }),
+      backgroundImage: theme => ({
+        'left-panel': "url('../img/loginPanelBgc.png')"
       }),
       fontFamily:{
         'body': ['Roboto', 'sans-serif']
