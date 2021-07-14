3dee59fb366e709d669ab2395ab02755a5008750
Nikodemek <nikokwiatek@gmail.com>
2021-07-15 00:17:51 +0200

    desktop view user page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      gridRowStart:{
+        '8': '8'
+      },
+      gridRowEnd:{
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': '11',
+        '12': '12',
+        '13': '13',
+      },
+      gridTemplateRows:{
+        '8': 'repeat(8, minmax(0, 1fr))'
+      },
       lineHeight:{
         '11': '4rem'
       },
